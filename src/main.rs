ad14e84046fca2e061322211d0cebe77464f3347
Ronnie <rjhj@protonmail.com>
2025-10-22 12:54:20 -0400

    playing around with it some more

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,107 @@
-use std::io::Error;
-use std::path::PathBuf;
-use std::fs;
-use std::process::exit;
+use std::{io::{BufRead, BufReader}, iter::Map};
+
 
 fn main() {
 
-    let cli = Cli::new(std::env::args().collect());
-
-    let word = Cli::parse(&cli).unwrap();
-
-    println!("Finished! {:?}", word);
+    let args:Vec<String> = std::env::args().collect();
+    let cli = Cli::new(args);
+    println!("{:?}", cli.deep_search());
 
 }
-#[derive(Debug)]
 struct Cli {
     pattern: String,
-    path: PathBuf,
+    path: std::path::PathBuf,
+
 }
+
 
 
 impl Cli {
     fn new(args: Vec<String>) -> Cli {
-        if args.len() < 3 {
-            Cli::error( 0)
-        }
         
-        let pattern =  args.iter().nth(1).unwrap().clone();
-        let path:PathBuf = PathBuf::from(args.iter().nth(2).unwrap().clone());
-        println!("path: {:?}", path);
-        Cli { pattern, path }
-    }
+        let mut iter = args.into_iter().skip(1);
+        let pattern = iter.next().expect("No pattern provided");
+        let path = iter.next().expect("No path provided").into();
 
-    fn parse(&self)  -> std::io::Result<String> {
-        let content = fs::read_to_string(self.path.as_path())?;
-        let mut word: String = String::new();
-        for line in content.lines() {
-            if(line.contains(self.pattern.as_str())) {
-                word = String::from(line);
+        Cli {
+            pattern,
+            path,
+        }
+    }
+       
+    fn quick_search(&self){
+
+    let file = std::fs::File::open(&self.path).expect("Could not open file");
+    let file_reader = BufReader::new(file);
+
+        for line in file_reader.lines() {
+            let line = line.expect("Could not read line");
+            if line.contains(&self.pattern) {
+                println!("{}", line);
             }
-        }
-        Ok(word)
-    }
 
-    fn error(code: i8)  {
-
-        match &code {
-            0 => {println!("[Not enough arguments]\nUsage : cargo run -- [pattern] [path]");
-                            exit(0)},
-            _ => println!("")
+ 
         }
     }
+
+    fn deep_search(&self) /* -> Vec<String> */ {
+        let root = std::path::Path::new("/");
+        let mut queue = Queue::new();
+
+        queue.enqueue(root.to_path_buf());
+
+        for dir in queue.dequeue() {
+           if dir.is_dir() {
+                for entry in std::fs::read_dir(dir).expect("Could not read directory") {
+                     let entry = entry.expect("Could not get directory entry");
+                     let path = entry.path();
+                     if path.is_dir() {
+                        println!("directory: {:?}", &path);
+                        queue.enqueue(path);
+                          
+                     } else {
+                            let file = std::fs::File::open(&path).expect("Could not open file");
+                            let file_reader = BufReader::new(file);
+
+                                for line in file_reader.lines() {
+                                    let line = line.expect("Could not read line");
+                                    if line.contains(&self.pattern) {
+                                        println!("Found in {:?}: {}", &path, line);
+                                    }
+                                }
+                          }
+                     }
+                }
+           }
+        }
+
+        
+
+    }
+
+
+struct Queue <T> {
+    items: Vec<T>
+}
+
+impl <T> Queue <T> {
+    fn new() -> Queue<T> {
+        Queue {
+            items: Vec::new()
+        }
+    }
+    fn enqueue(&mut self, item: T) {
+        self.items.push(item);
+    }
+    fn dequeue(&mut self) -> Option<T> {
+        if self.items.is_empty() {
+            None
+        } else {
+            Some(self.items.remove(0))
+        }
+    }
+      fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+   
 }
